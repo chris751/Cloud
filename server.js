8d62dd0175be7c7fa180e4a81acbb8a4fab96d47
Christian Maigaard <maigaard.christian@gmail.com>
2017-11-12 14:50:36 +0100

    leftovers

--- server.js
+++ server.js
@@ -5,17 +5,25 @@ var request = require('request');
 var google = require('googleapis');
 var GoogleAuth = require('google-auth-library');
 var exphbs = require('express-handlebars');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
 var handlebars = require('./helpers/handlebars.js')(exphbs);
 var googleHelpers = require('./helpers/googleHelpers');
 var settingsHelper = require('./settings/settingsWriter');
 
+
 var app = express();
 app.use(cors());
+
+
+
+var url = require('url');
 
 app.engine('hbs', handlebars.engine);
 
 app.set('view engine', 'hbs');
+app.set('port', (process.env.PORT || 5000));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -24,15 +32,20 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json())
 
-var OAuth2 = google.auth.OAuth2;
+app.use(cookieParser());
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 
 var TOKEN_PATH = './users/tokens.json';
+var ENVIROMENT_URL = 'https://f1a545e0.ngrok.io';
+var CALLBACK_URL = `${ENVIROMENT_URL}/auth/google/callback`;
 
-var oauth2Client = new OAuth2(
-  '260827620000-n22fb3grjj24e46jr6ul88tnngfh10bd.apps.googleusercontent.com',
-  '-0SPco82-qUcX42eMOJpGqnw',
-  'http://localhost:8080/auth/google/callback'
-);
+var OAuth2 = google.auth.OAuth2;
 
 // generate a url that asks permissions for Google+ and Google Calendar scopes
 var scopes = [
@@ -40,56 +53,102 @@ var scopes = [
   'https://www.googleapis.com/auth/calendar'
 ];
 
+
 app.get('/', function (req, res) {
   res.render('home.hbs');
 });
 
 app.get('/login', function (req, res) {
-  var url = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: scopes,
-  });
-  res.redirect(url);
+  getClient(function (oauth2Client) {
+
+    var url = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: scopes,
+    });
+    res.redirect(url);
+  })
 });
 
 app.get('/auth/google/callback', function (req, res) {
-  var code = req.query['code'];
-  console.log(code);
 
-  oauth2Client.getToken(code, function (err, tokens) {
-    // Now tokens contains an access_token and an optional refresh_token. Save them.
-    if (!err) {
-      console.log(tokens);
-      oauth2Client.setCredentials(tokens);
-      googleHelpers.listCalendarEvents(oauth2Client, google);
-      //storeToken(tokens);
-      res.redirect('/profile'); // when authenticated send to profile site
-    } else {
-      res.send('error');
-    }
+
+  var code = req.query['code'];
+  //console.log(code);
+
+
+  getClient(function (client) {
+    var oauth2Client = client;
+
+
+
+    oauth2Client.getToken(code, function (err, tokens) {
+      // Now tokens contains an access_token and an optional refresh_token. Save them.
+      if (!err) {
+        //console.log(tokens);
+        oauth2Client.setCredentials(tokens);
+        googleHelpers.listCalendarEvents(oauth2Client, google);
+        gAuth(oauth2Client, function (userInfo) {
+          if (userInfo !== 'error') {
+            console.log(userInfo.id);
+            console.log(userInfo.name);
+            req.session.user_id = userInfo.id;
+            req.session.name = userInfo.name;
+            res.redirect('/profile'); // when authenticated send to profile site
+          }
+        })
+      } else {
+        res.send('error');
+      }
+    });
   });
 });
 
-app.get('/profile', function (req, res) {
+
+var gAuth = function (oauth2Client, callback) {
   google.oauth2("v2").userinfo.v2.me.get({
     auth: oauth2Client
   }, (e, profile) => {
     if (!e) {
-      res.render('profile.hbs', {
-        name: profile.name,
-        id: profile.id
-      });
+      console.log(profile.id);
+      callback(profile);
     } else {
-      res.redirect('/login'); // the user wasn't logged in, so we redirect them to the login page
+      callback('error');
     }
   });
+}
+
+var getClient = function (callback) {
+  var oauth2Client = new OAuth2(
+    '260827620000-n22fb3grjj24e46jr6ul88tnngfh10bd.apps.googleusercontent.com',
+    '-0SPco82-qUcX42eMOJpGqnw',
+    CALLBACK_URL
+  );
+  console.log(ENVIROMENT_URL);
+  console.log(CALLBACK_URL);
+  callback(oauth2Client);
+}
+
+app.get('/profile', function (req, res) {
+  console.log(req.session.user_id);
+  console.log(req.session.name);
+  if (req.session.user_id) {
+    res.render('profile.hbs', {
+      name: req.session.name,
+      id: req.session.user_id
+    });
+  }else {
+    res.redirect('/');
+  }
 });
 
 app.post('/settings', function (request, res) {
-  settingsHelper.addNote(request.body, function response(){
+  console.log(request.body);
+  settingsHelper.addNote(request.body, function response() {
     res.send('saved!');
   });
 });
 
-app.listen(8080);
-console.log('server is up on port 8080');
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
--- settings/settingsWriter.js
+++ settings/settingsWriter.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var _  = require('lodash');
 
   var fetchNotes = () => {
     try {
@@ -10,7 +11,8 @@ var fs = require('fs');
   };
 
   var saveNotes = (notes) => {
-    fs.writeFileSync('./settings/userSettings.json', JSON.stringify(notes));
+    fs.writeFileSync('./settings/userSettings.json', JSON.stringify(notes, null, 2));
+    console.log(notes);
   };
 
   exports.addNote = function (body, callback) {
@@ -18,15 +20,23 @@ var fs = require('fs');
     var settings = fetchNotes();
 
     if (settings.length > 0) { // settings file is not empty 
-      for (i = settings.length - 1; i >= 0; --i) {
+      for (i = settings.length-1; i >= 0; --i) {
+        //console.log(i);
+        //console.log(settings);
         if (settings[i].profile_id !== body.profile_id) { // a new user is here!
+          //console.log(' if');
           addIt(body, settings, callback); 
+          
+          
         } else if (settings[i].profile_id == body.profile_id) { // user is already there, but some setting has been changed
+          //console.log('else if');
           settings.splice(i, 1); // delete it
           addIt(body, settings, callback); // add it
+          
         }
       }
     } else { // settings file is empty
+      //console.log('else');
       addIt(body, settings, callback);
     }
   }
@@ -34,6 +44,8 @@ var fs = require('fs');
   var addIt = function (body, settings, callback) {
     console.log('adding it');
     settings.push(body);
-    saveNotes(settings);
-    callback();
+    var uniqe = _.uniqBy(settings, 'profile_id');
+    //console.log(uniqe);
+    saveNotes(uniqe);
+    return callback();
   }
