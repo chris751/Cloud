65f3591a78faf0dda8b451ad1bb766dcf36def7e
Christian Maigaard <maigaard.christian@gmail.com>
2017-11-17 13:56:47 +0100

    fixed endless loop

--- engine/lightEngine.js
+++ engine/lightEngine.js
@@ -6,19 +6,19 @@ var start = (settings) => {
     checkForChanges(settings);
 }
 
+
+var newAddressArray = [];
+var oldAddressArray = [];
+
 var checkForChanges = (settings) => {
-    newAddressArray = [];
-    oldAddressArray = [];
     for (i = settings.length - 1; i >= 0; --i) {
         if (settings[i].mac_address) {
             newAddressArray.push(settings[i].mac_address);
-            
         }
     }
     if(newAddressArray != oldAddressArray){
         communication.sendMacToPi(newAddressArray);
     }
-
     oldAddressArray = newAddressArray;
 }
 
